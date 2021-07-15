444399e800b9bc4fbe25ea57e057fb4a0b77c5dd
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-07-15 18:53:51 +0200

    Revert

--- elliptic/elliptic.go
+++ elliptic/elliptic.go
@@ -14,12 +14,9 @@ package elliptic
 // reverse the transform than to operate in affine coordinates.
 
 import (
-	"fmt"
 	"io"
 	"math/big"
 	"sync"
-
-	"github.com/cronokirby/safenum"
 )
 
 // A Curve represents a short-form Weierstrass curve with a=-3.
@@ -31,27 +28,27 @@ type Curve interface {
 	// Params returns the parameters for the curve.
 	Params() *CurveParams
 	// IsOnCurve reports whether the given (x,y) lies on the curve.
-	IsOnCurve(x, y *safenum.Nat) bool
+	IsOnCurve(x, y *big.Int) bool
 	// Add returns the sum of (x1,y1) and (x2,y2)
-	Add(x1, y1, x2, y2 *safenum.Nat) (x, y *safenum.Nat)
+	Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int)
 	// Double returns 2*(x,y)
-	Double(x1, y1 *safenum.Nat) (x, y *safenum.Nat)
+	Double(x1, y1 *big.Int) (x, y *big.Int)
 	// ScalarMult returns k*(Bx,By) where k is a number in big-endian form.
-	ScalarMult(x1, y1 *safenum.Nat, k []byte) (x, y *safenum.Nat)
+	ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int)
 	// ScalarBaseMult returns k*G, where G is the base point of the group
 	// and k is an integer in big-endian form.
-	ScalarBaseMult(k []byte) (x, y *safenum.Nat)
+	ScalarBaseMult(k []byte) (x, y *big.Int)
 }
 
 // CurveParams contains the parameters of an elliptic curve and also provides
 // a generic, non-constant time implementation of Curve.
 type CurveParams struct {
-	P       *safenum.Modulus // the order of the underlying field
-	N       *safenum.Modulus // the order of the base point
-	B       *safenum.Nat     // the constant of the curve equation
-	Gx, Gy  *safenum.Nat     // (x,y) of the base point
-	BitSize int              // the size of the underlying field
-	Name    string           // the canonical name of the curve
+	P       *big.Int // the order of the underlying field
+	N       *big.Int // the order of the base point
+	B       *big.Int // the constant of the curve equation
+	Gx, Gy  *big.Int // (x,y) of the base point
+	BitSize int      // the size of the underlying field
+	Name    string   // the canonical name of the curve
 }
 
 func (curve *CurveParams) Params() *CurveParams {
@@ -59,56 +56,58 @@ func (curve *CurveParams) Params() *CurveParams {
 }
 
 // polynomial returns x³ - 3x + b.
-func (curve *CurveParams) polynomial(x *safenum.Nat) *safenum.Nat {
-	x3 := new(safenum.Nat).ModMul(x, x, curve.P)
-	x3.ModMul(x3, x, curve.P)
+func (curve *CurveParams) polynomial(x *big.Int) *big.Int {
+	x3 := new(big.Int).Mul(x, x)
+	x3.Mul(x3, x)
 
-	threeX := new(safenum.Nat).ModAdd(x, x, curve.P)
-	threeX.ModAdd(threeX, x, curve.P)
+	threeX := new(big.Int).Lsh(x, 1)
+	threeX.Add(threeX, x)
 
-	x3.ModSub(x3, threeX, curve.P)
-	x3.ModAdd(x3, curve.B, curve.P)
+	x3.Sub(x3, threeX)
+	x3.Add(x3, curve.B)
+	x3.Mod(x3, curve.P)
 
 	return x3
 }
 
-func (curve *CurveParams) IsOnCurve(x, y *safenum.Nat) bool {
+func (curve *CurveParams) IsOnCurve(x, y *big.Int) bool {
 	// y² = x³ - 3x + b
-	y2 := new(safenum.Nat).ModMul(y, y, curve.P)
-	fmt.Println("y2", y2)
-	fmt.Println("f(x)", curve.polynomial(x))
+	y2 := new(big.Int).Mul(y, y)
+	y2.Mod(y2, curve.P)
 
-	return curve.polynomial(x).Eq(y2) == 1
+	return curve.polynomial(x).Cmp(y2) == 0
 }
 
 // zForAffine returns a Jacobian Z value for the affine point (x, y). If x and
 // y are zero, it assumes that they represent the point at infinity because (0,
 // 0) is not on the any of the curves handled here.
-func zForAffine(x, y *safenum.Nat) *safenum.Nat {
-	z := new(safenum.Nat)
-	if x.EqZero() != 1 || y.EqZero() != 1 {
-		z.SetUint64(1)
+func zForAffine(x, y *big.Int) *big.Int {
+	z := new(big.Int)
+	if x.Sign() != 0 || y.Sign() != 0 {
+		z.SetInt64(1)
 	}
 	return z
 }
 
 // affineFromJacobian reverses the Jacobian transform. See the comment at the
 // top of the file. If the point is ∞ it returns 0, 0.
-func (curve *CurveParams) affineFromJacobian(x, y, z *safenum.Nat) (xOut, yOut *safenum.Nat) {
-	if z.EqZero() == 1 {
-		return new(safenum.Nat), new(safenum.Nat)
+func (curve *CurveParams) affineFromJacobian(x, y, z *big.Int) (xOut, yOut *big.Int) {
+	if z.Sign() == 0 {
+		return new(big.Int), new(big.Int)
 	}
 
-	zinv := new(safenum.Nat).ModInverse(z, curve.P)
-	zinvsq := new(safenum.Nat).ModMul(zinv, zinv, curve.P)
+	zinv := new(big.Int).ModInverse(z, curve.P)
+	zinvsq := new(big.Int).Mul(zinv, zinv)
 
-	xOut = new(safenum.Nat).ModMul(x, zinvsq, curve.P)
-	zinvsq.ModMul(zinvsq, zinv, curve.P)
-	yOut = new(safenum.Nat).ModMul(y, zinvsq, curve.P)
+	xOut = new(big.Int).Mul(x, zinvsq)
+	xOut.Mod(xOut, curve.P)
+	zinvsq.Mul(zinvsq, zinv)
+	yOut = new(big.Int).Mul(y, zinvsq)
+	yOut.Mod(yOut, curve.P)
 	return
 }
 
-func (curve *CurveParams) Add(x1, y1, x2, y2 *safenum.Nat) (*safenum.Nat, *safenum.Nat) {
+func (curve *CurveParams) Add(x1, y1, x2, y2 *big.Int) (*big.Int, *big.Int) {
 	z1 := zForAffine(x1, y1)
 	z2 := zForAffine(x2, y2)
 	return curve.affineFromJacobian(curve.addJacobian(x1, y1, z1, x2, y2, z2))
@@ -116,117 +115,151 @@ func (curve *CurveParams) Add(x1, y1, x2, y2 *safenum.Nat) (*safenum.Nat, *safen
 
 // addJacobian takes two points in Jacobian coordinates, (x1, y1, z1) and
 // (x2, y2, z2) and returns their sum, also in Jacobian form.
-func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *safenum.Nat) (*safenum.Nat, *safenum.Nat, *safenum.Nat) {
+func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (*big.Int, *big.Int, *big.Int) {
 	// See https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#addition-add-2007-bl
-	x3, y3, z3 := new(safenum.Nat), new(safenum.Nat), new(safenum.Nat)
-	if z1.EqZero() == 1 {
-		x3.SetNat(x2)
-		y3.SetNat(y2)
-		z3.SetNat(z2)
+	x3, y3, z3 := new(big.Int), new(big.Int), new(big.Int)
+	if z1.Sign() == 0 {
+		x3.Set(x2)
+		y3.Set(y2)
+		z3.Set(z2)
 		return x3, y3, z3
 	}
-	if z2.EqZero() == 1 {
-		x3.SetNat(x1)
-		y3.SetNat(y1)
-		z3.SetNat(z1)
+	if z2.Sign() == 0 {
+		x3.Set(x1)
+		y3.Set(y1)
+		z3.Set(z1)
 		return x3, y3, z3
 	}
 
-	z1z1 := new(safenum.Nat).ModMul(z1, z1, curve.P)
-	z2z2 := new(safenum.Nat).ModMul(z2, z2, curve.P)
+	z1z1 := new(big.Int).Mul(z1, z1)
+	z1z1.Mod(z1z1, curve.P)
+	z2z2 := new(big.Int).Mul(z2, z2)
+	z2z2.Mod(z2z2, curve.P)
 
-	u1 := new(safenum.Nat).ModMul(x1, z2z2, curve.P)
-	u2 := new(safenum.Nat).ModMul(x2, z1z1, curve.P)
-	h := new(safenum.Nat).ModSub(u2, u1, curve.P)
-	xEqual := h.EqZero() == 1
-	i := new(safenum.Nat).ModAdd(h, h, curve.P)
-	i.ModMul(i, i, curve.P)
-	j := new(safenum.Nat).ModMul(h, i, curve.P)
+	u1 := new(big.Int).Mul(x1, z2z2)
+	u1.Mod(u1, curve.P)
+	u2 := new(big.Int).Mul(x2, z1z1)
+	u2.Mod(u2, curve.P)
+	h := new(big.Int).Sub(u2, u1)
+	xEqual := h.Sign() == 0
+	if h.Sign() == -1 {
+		h.Add(h, curve.P)
+	}
+	i := new(big.Int).Lsh(h, 1)
+	i.Mul(i, i)
+	j := new(big.Int).Mul(h, i)
 
-	s1 := new(safenum.Nat).ModMul(y1, z2, curve.P)
-	s1.ModMul(s1, z2z2, curve.P)
-	s2 := new(safenum.Nat).ModMul(y2, z1, curve.P)
-	s2.ModMul(s2, z1z1, curve.P)
-	r := new(safenum.Nat).ModSub(s2, s1, curve.P)
-	yEqual := r.EqZero() == 1
+	s1 := new(big.Int).Mul(y1, z2)
+	s1.Mul(s1, z2z2)
+	s1.Mod(s1, curve.P)
+	s2 := new(big.Int).Mul(y2, z1)
+	s2.Mul(s2, z1z1)
+	s2.Mod(s2, curve.P)
+	r := new(big.Int).Sub(s2, s1)
+	if r.Sign() == -1 {
+		r.Add(r, curve.P)
+	}
+	yEqual := r.Sign() == 0
 	if xEqual && yEqual {
 		return curve.doubleJacobian(x1, y1, z1)
 	}
-	r.ModAdd(r, r, curve.P)
-	v := new(safenum.Nat).ModMul(u1, i, curve.P)
+	r.Lsh(r, 1)
+	v := new(big.Int).Mul(u1, i)
 
-	x3.SetNat(r)
-	x3.ModMul(x3, x3, curve.P)
-	x3.ModSub(x3, j, curve.P)
-	x3.ModSub(x3, v, curve.P)
-	x3.ModSub(x3, v, curve.P)
+	x3.Set(r)
+	x3.Mul(x3, x3)
+	x3.Sub(x3, j)
+	x3.Sub(x3, v)
+	x3.Sub(x3, v)
+	x3.Mod(x3, curve.P)
 
-	y3.SetNat(r)
-	v.ModSub(v, x3, curve.P)
-	y3.ModMul(y3, v, curve.P)
-	s1.ModMul(s1, j, curve.P)
-	s1.ModAdd(s1, s1, curve.P)
-	y3.ModSub(y3, s1, curve.P)
+	y3.Set(r)
+	v.Sub(v, x3)
+	y3.Mul(y3, v)
+	s1.Mul(s1, j)
+	s1.Lsh(s1, 1)
+	y3.Sub(y3, s1)
+	y3.Mod(y3, curve.P)
 
-	z3.ModAdd(z1, z2, curve.P)
-	z3.ModMul(z3, z3, curve.P)
-	z3.ModSub(z3, z1z1, curve.P)
-	z3.ModSub(z3, z2z2, curve.P)
-	z3.ModMul(z3, h, curve.P)
+	z3.Add(z1, z2)
+	z3.Mul(z3, z3)
+	z3.Sub(z3, z1z1)
+	z3.Sub(z3, z2z2)
+	z3.Mul(z3, h)
+	z3.Mod(z3, curve.P)
 
 	return x3, y3, z3
 }
 
-func (curve *CurveParams) Double(x1, y1 *safenum.Nat) (*safenum.Nat, *safenum.Nat) {
+func (curve *CurveParams) Double(x1, y1 *big.Int) (*big.Int, *big.Int) {
 	z1 := zForAffine(x1, y1)
 	return curve.affineFromJacobian(curve.doubleJacobian(x1, y1, z1))
 }
 
 // doubleJacobian takes a point in Jacobian coordinates, (x, y, z), and
 // returns its double, also in Jacobian form.
-func (curve *CurveParams) doubleJacobian(x, y, z *safenum.Nat) (*safenum.Nat, *safenum.Nat, *safenum.Nat) {
+func (curve *CurveParams) doubleJacobian(x, y, z *big.Int) (*big.Int, *big.Int, *big.Int) {
 	// See https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#doubling-dbl-2001-b
-	delta := new(safenum.Nat).ModMul(z, z, curve.P)
-	gamma := new(safenum.Nat).ModMul(y, y, curve.P)
-	alpha := new(safenum.Nat).ModSub(x, delta, curve.P)
-	alpha2 := new(safenum.Nat).ModAdd(x, delta, curve.P)
-	alpha.ModMul(alpha, alpha2, curve.P)
-	alpha2.SetNat(alpha)
-	alpha.ModAdd(alpha, alpha, curve.P)
-	alpha.ModAdd(alpha, alpha2, curve.P)
+	delta := new(big.Int).Mul(z, z)
+	delta.Mod(delta, curve.P)
+	gamma := new(big.Int).Mul(y, y)
+	gamma.Mod(gamma, curve.P)
+	alpha := new(big.Int).Sub(x, delta)
+	if alpha.Sign() == -1 {
+		alpha.Add(alpha, curve.P)
+	}
+	alpha2 := new(big.Int).Add(x, delta)
+	alpha.Mul(alpha, alpha2)
+	alpha2.Set(alpha)
+	alpha.Lsh(alpha, 1)
+	alpha.Add(alpha, alpha2)
 
-	beta := alpha2.ModMul(x, gamma, curve.P)
+	beta := alpha2.Mul(x, gamma)
 
-	x3 := new(safenum.Nat).ModMul(alpha, alpha, curve.P)
-	beta8 := new(safenum.Nat).ModAdd(beta, beta, curve.P)
-	beta8.ModAdd(beta, beta, curve.P)
-	beta8.ModAdd(beta, beta, curve.P)
-	x3.ModSub(x3, beta8, curve.P)
+	x3 := new(big.Int).Mul(alpha, alpha)
+	beta8 := new(big.Int).Lsh(beta, 3)
+	beta8.Mod(beta8, curve.P)
+	x3.Sub(x3, beta8)
+	if x3.Sign() == -1 {
+		x3.Add(x3, curve.P)
+	}
+	x3.Mod(x3, curve.P)
 
-	z3 := new(safenum.Nat).ModAdd(y, z, curve.P)
-	z3.ModMul(z3, z3, curve.P)
-	z3.ModSub(z3, gamma, curve.P)
-	z3.ModSub(z3, delta, curve.P)
+	z3 := new(big.Int).Add(y, z)
+	z3.Mul(z3, z3)
+	z3.Sub(z3, gamma)
+	if z3.Sign() == -1 {
+		z3.Add(z3, curve.P)
+	}
+	z3.Sub(z3, delta)
+	if z3.Sign() == -1 {
+		z3.Add(z3, curve.P)
+	}
 	z3.Mod(z3, curve.P)
 
-	beta.ModAdd(beta, beta, curve.P)
-	beta.ModAdd(beta, beta, curve.P)
-	beta.ModSub(beta, x3, curve.P)
-	y3 := alpha.ModMul(alpha, beta, curve.P)
+	beta.Lsh(beta, 2)
+	beta.Sub(beta, x3)
+	if beta.Sign() == -1 {
+		beta.Add(beta, curve.P)
+	}
+	y3 := alpha.Mul(alpha, beta)
 
-	gamma.ModMul(gamma, gamma, curve.P)
-	gamma.ModAdd(gamma, gamma, curve.P)
-	gamma.ModAdd(gamma, gamma, curve.P)
-	gamma.ModAdd(gamma, gamma, curve.P)
+	gamma.Mul(gamma, gamma)
+	gamma.Lsh(gamma, 3)
+	gamma.Mod(gamma, curve.P)
 
-	y3.ModSub(y3, gamma, curve.P)
+	y3.Sub(y3, gamma)
+	if y3.Sign() == -1 {
+		y3.Add(y3, curve.P)
+	}
+	y3.Mod(y3, curve.P)
 
 	return x3, y3, z3
 }
 
-func (curve *CurveParams) ScalarMult(Bx, By *safenum.Nat, k []byte) (*safenum.Nat, *safenum.Nat) {
-	Bz := new(safenum.Nat).SetUint64(1)
-	x, y, z := new(safenum.Nat), new(safenum.Nat), new(safenum.Nat)
+func (curve *CurveParams) ScalarMult(Bx, By *big.Int, k []byte) (*big.Int, *big.Int) {
+	Bz := new(big.Int).SetInt64(1)
+	x, y, z := new(big.Int), new(big.Int), new(big.Int)
 
 	for _, byte := range k {
 		for bitNum := 0; bitNum < 8; bitNum++ {
@@ -241,7 +274,7 @@ func (curve *CurveParams) ScalarMult(Bx, By *safenum.Nat, k []byte) (*safenum.Na
 	return curve.affineFromJacobian(x, y, z)
 }
 
-func (curve *CurveParams) ScalarBaseMult(k []byte) (*safenum.Nat, *safenum.Nat) {
+func (curve *CurveParams) ScalarBaseMult(k []byte) (*big.Int, *big.Int) {
 	return curve.ScalarMult(curve.Gx, curve.Gy, k)
 }
 
@@ -249,7 +282,7 @@ var mask = []byte{0xff, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, 0x7f}
 
 // GenerateKey returns a public/private key pair. The private key is
 // generated using the given reader, which must return random data.
-func GenerateKey(curve Curve, rand io.Reader) (priv []byte, x, y *safenum.Nat, err error) {
+func GenerateKey(curve Curve, rand io.Reader) (priv []byte, x, y *big.Int, err error) {
 	N := curve.Params().N
 	bitSize := N.BitLen()
 	byteLen := (bitSize + 7) / 8
@@ -268,8 +301,7 @@ func GenerateKey(curve Curve, rand io.Reader) (priv []byte, x, y *safenum.Nat, e
 		priv[1] ^= 0x42
 
 		// If the scalar is out of range, sample another random number.
-		_, _, lt := new(safenum.Nat).SetBytes(priv).CmpMod(N)
-		if lt != 1 {
+		if new(big.Int).SetBytes(priv).Cmp(N) >= 0 {
 			continue
 		}
 
@@ -280,7 +312,7 @@ func GenerateKey(curve Curve, rand io.Reader) (priv []byte, x, y *safenum.Nat, e
 
 // Marshal converts a point on the curve into the uncompressed form specified in
 // section 4.3.6 of ANSI X9.62.
-func Marshal(curve Curve, x, y *safenum.Nat) []byte {
+func Marshal(curve Curve, x, y *big.Int) []byte {
 	byteLen := (curve.Params().BitSize + 7) / 8
 
 	ret := make([]byte, 1+2*byteLen)
@@ -288,17 +320,16 @@ func Marshal(curve Curve, x, y *safenum.Nat) []byte {
 
 	x.FillBytes(ret[1 : 1+byteLen])
 	y.FillBytes(ret[1+byteLen : 1+2*byteLen])
-	fmt.Println("Marshal", x, y)
 
 	return ret
 }
 
 // MarshalCompressed converts a point on the curve into the compressed form
 // specified in section 4.3.6 of ANSI X9.62.
-func MarshalCompressed(curve Curve, x, y *safenum.Nat) []byte {
+func MarshalCompressed(curve Curve, x, y *big.Int) []byte {
 	byteLen := (curve.Params().BitSize + 7) / 8
 	compressed := make([]byte, 1+byteLen)
-	compressed[0] = byte(y.Byte(0)&1) | 2
+	compressed[0] = byte(y.Bit(0)) | 2
 	x.FillBytes(compressed[1:])
 	return compressed
 }
@@ -306,7 +337,7 @@ func MarshalCompressed(curve Curve, x, y *safenum.Nat) []byte {
 // Unmarshal converts a point, serialized by Marshal, into an x, y pair.
 // It is an error if the point is not in uncompressed form or is not on the curve.
 // On error, x = nil.
-func Unmarshal(curve Curve, data []byte) (x, y *safenum.Nat) {
+func Unmarshal(curve Curve, data []byte) (x, y *big.Int) {
 	byteLen := (curve.Params().BitSize + 7) / 8
 	if len(data) != 1+2*byteLen {
 		return nil, nil
@@ -315,20 +346,12 @@ func Unmarshal(curve Curve, data []byte) (x, y *safenum.Nat) {
 		return nil, nil
 	}
 	p := curve.Params().P
-	x = new(safenum.Nat).SetBytes(data[1 : 1+byteLen])
-	fmt.Println("unmarshaled x:", x)
-	_, _, lt := x.CmpMod(p)
-	if lt != 1 {
-		return nil, nil
-	}
-	y = new(safenum.Nat).SetBytes(data[1+byteLen:])
-	fmt.Println("unmarshaled y:", y)
-	_, _, lt = y.CmpMod(p)
-	if lt != 1 {
+	x = new(big.Int).SetBytes(data[1 : 1+byteLen])
+	y = new(big.Int).SetBytes(data[1+byteLen:])
+	if x.Cmp(p) >= 0 || y.Cmp(p) >= 0 {
 		return nil, nil
 	}
 	if !curve.IsOnCurve(x, y) {
-		fmt.Println("not on curve :(")
 		return nil, nil
 	}
 	return
@@ -337,7 +360,7 @@ func Unmarshal(curve Curve, data []byte) (x, y *safenum.Nat) {
 // UnmarshalCompressed converts a point, serialized by MarshalCompressed, into an x, y pair.
 // It is an error if the point is not in compressed form or is not on the curve.
 // On error, x = nil.
-func UnmarshalCompressed(curve Curve, data []byte) (x, y *safenum.Nat) {
+func UnmarshalCompressed(curve Curve, data []byte) (x, y *big.Int) {
 	byteLen := (curve.Params().BitSize + 7) / 8
 	if len(data) != 1+byteLen {
 		return nil, nil
@@ -346,9 +369,8 @@ func UnmarshalCompressed(curve Curve, data []byte) (x, y *safenum.Nat) {
 		return nil, nil
 	}
 	p := curve.Params().P
-	x = new(safenum.Nat).SetBytes(data[1:])
-	_, _, lt := x.CmpMod(p)
-	if lt != 1 {
+	x = new(big.Int).SetBytes(data[1:])
+	if x.Cmp(p) >= 0 {
 		return nil, nil
 	}
 	// y² = x³ - 3x + b
@@ -357,8 +379,8 @@ func UnmarshalCompressed(curve Curve, data []byte) (x, y *safenum.Nat) {
 	if y == nil {
 		return nil, nil
 	}
-	if byte(y.Byte(0)&1) != data[0]&1 {
-		y.ModSub(new(safenum.Nat), y, p)
+	if byte(y.Bit(0)) != data[0]&1 {
+		y.Neg(y).Mod(y, p)
 	}
 	if !curve.IsOnCurve(x, y) {
 		return nil, nil
@@ -373,24 +395,14 @@ func initAll() {
 	initP384()
 }
 
-func natFromString(s string, base int) (*safenum.Nat, bool) {
-	x, success := new(big.Int).SetString(s, base)
-	return new(safenum.Nat).SetBig(x, x.BitLen()), success
-}
-
-func modFromString(s string, base int) (*safenum.Modulus, bool) {
-	x, success := natFromString(s, base)
-	return safenum.ModulusFromNat(x), success
-}
-
 func initP384() {
 	// See FIPS 186-3, section D.2.4
 	p384 = &CurveParams{Name: "P-384"}
-	p384.P, _ = modFromString("39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112319", 10)
-	p384.N, _ = modFromString("39402006196394479212279040100143613805079739270465446667946905279627659399113263569398956308152294913554433653942643", 10)
-	p384.B, _ = natFromString("b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef", 16)
-	p384.Gx, _ = natFromString("aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7", 16)
-	p384.Gy, _ = natFromString("3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f", 16)
+	p384.P, _ = new(big.Int).SetString("39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112319", 10)
+	p384.N, _ = new(big.Int).SetString("39402006196394479212279040100143613805079739270465446667946905279627659399113263569398956308152294913554433653942643", 10)
+	p384.B, _ = new(big.Int).SetString("b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef", 16)
+	p384.Gx, _ = new(big.Int).SetString("aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7", 16)
+	p384.Gy, _ = new(big.Int).SetString("3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f", 16)
 	p384.BitSize = 384
 }
 
--- elliptic/elliptic_test.go
+++ elliptic/elliptic_test.go
@@ -7,11 +7,8 @@ package elliptic
 import (
 	"bytes"
 	"crypto/rand"
-	"fmt"
-	mrand "math/rand"
+	"math/big"
 	"testing"
-
-	"github.com/cronokirby/safenum"
 )
 
 // genericParamsForCurve returns the dereferenced CurveParams for
@@ -54,7 +51,7 @@ func TestOnCurve(t *testing.T) {
 
 func TestOffCurve(t *testing.T) {
 	testAllCurves(t, func(t *testing.T, curve Curve) {
-		x, y := new(safenum.Nat).SetUint64(1), new(safenum.Nat).SetUint64(1)
+		x, y := new(big.Int).SetInt64(1), new(big.Int).SetInt64(1)
 		if curve.IsOnCurve(x, y) {
 			t.Errorf("point off curve is claimed to be on the curve")
 		}
@@ -73,20 +70,19 @@ func TestInfinity(t *testing.T) {
 func testInfinity(t *testing.T, curve Curve) {
 	_, x, y, _ := GenerateKey(curve, rand.Reader)
 	x, y = curve.ScalarMult(x, y, curve.Params().N.Bytes())
-	fmt.Println("x", x, "y", y)
-	if x.EqZero() == 0 || y.EqZero() == 0 {
+	if x.Sign() != 0 || y.Sign() != 0 {
 		t.Errorf("x^q != ∞")
 	}
 
 	x, y = curve.ScalarBaseMult([]byte{0})
-	if x.EqZero() == 0 || y.EqZero() == 0 {
+	if x.Sign() != 0 || y.Sign() != 0 {
 		t.Errorf("b^0 != ∞")
-		x.SetUint64(0)
-		y.SetUint64(0)
+		x.SetInt64(0)
+		y.SetInt64(0)
 	}
 
 	x2, y2 := curve.Double(x, y)
-	if x2.EqZero() == 0 || y2.EqZero() == 0 {
+	if x2.Sign() != 0 || y2.Sign() != 0 {
 		t.Errorf("2∞ != ∞")
 	}
 
@@ -94,12 +90,12 @@ func testInfinity(t *testing.T, curve Curve) {
 	baseY := curve.Params().Gy
 
 	x3, y3 := curve.Add(baseX, baseY, x, y)
-	if x3.Eq(baseX) != 1 || y3.Eq(baseY) != 1 {
+	if x3.Cmp(baseX) != 0 || y3.Cmp(baseY) != 0 {
 		t.Errorf("x+∞ != x")
 	}
 
 	x4, y4 := curve.Add(x, y, baseX, baseY)
-	if x4.Eq(baseX) != 1 || y4.Eq(baseY) != 1 {
+	if x4.Cmp(baseX) != 0 || y4.Cmp(baseY) != 0 {
 		t.Errorf("∞+x != x")
 	}
 
@@ -110,7 +106,7 @@ func testInfinity(t *testing.T, curve Curve) {
 
 func TestMarshal(t *testing.T) {
 	testAllCurves(t, func(t *testing.T, curve Curve) {
-		_, x, y, err := GenerateKey(curve, mrand.New(mrand.NewSource(0)))
+		_, x, y, err := GenerateKey(curve, rand.Reader)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -119,7 +115,7 @@ func TestMarshal(t *testing.T) {
 		if xx == nil {
 			t.Fatal("failed to unmarshal")
 		}
-		if xx.Eq(x) != 1 || yy.Eq(y) != 1 {
+		if xx.Cmp(x) != 0 || yy.Cmp(y) != 0 {
 			t.Fatal("unmarshal returned different values")
 		}
 	})
@@ -137,7 +133,7 @@ func testUnmarshalToLargeCoordinates(t *testing.T, curve Curve) {
 	// Set x to be greater than curve's parameter P – specifically, to P+5.
 	// Set y to mod_sqrt(x^3 - 3x + B)) so that (x mod P = 5 , y) is on the
 	// curve.
-	x := new(safenum.Nat).Add(new(safenum.Nat).SetBytes(p.Bytes()), new(safenum.Nat).SetUint64(5), p.BitLen()+3)
+	x := new(big.Int).Add(p, big.NewInt(5))
 	y := curve.Params().polynomial(x)
 	y.ModSqrt(y, p)
 
@@ -166,7 +162,7 @@ func TestMarshalCompressed(t *testing.T) {
 
 }
 
-func testMarshalCompressed(t *testing.T, curve Curve, x, y *safenum.Nat, want []byte) {
+func testMarshalCompressed(t *testing.T, curve Curve, x, y *big.Int, want []byte) {
 	if !curve.IsOnCurve(x, y) {
 		t.Fatal("invalid test point")
 	}
@@ -183,7 +179,7 @@ func testMarshalCompressed(t *testing.T, curve Curve, x, y *safenum.Nat, want []
 	if !curve.IsOnCurve(X, Y) {
 		t.Error("UnmarshalCompressed returned a point not on the curve")
 	}
-	if X.Eq(x) != 1 || Y.Eq(y) != 1 {
+	if X.Cmp(x) != 0 || Y.Cmp(y) != 0 {
 		t.Errorf("point did not round-trip correctly: got (%v, %v), want (%v, %v)", X, Y, x, y)
 	}
 }
@@ -211,7 +207,7 @@ func BenchmarkScalarBaseMult(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			x, _ := curve.ScalarBaseMult(priv)
 			// Prevent the compiler from optimizing out the operation.
-			priv[0] ^= byte(x.Byte(0))
+			priv[0] ^= byte(x.Bits()[0])
 		}
 	})
 }
