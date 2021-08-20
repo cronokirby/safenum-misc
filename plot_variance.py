5c982a8d7bd7ee007e39794a86cd56855457be79
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-20 18:21:45 +0200

    Update plot calculation

--- plot_variance.py
+++ plot_variance.py
@@ -4,35 +4,44 @@ import seaborn as sns
 
 
 def modaddplot(df):
-    preproc = pd.DataFrame({
-        'Bits': list(df[df.method == 'ModAddBig'].bits),
-        'Big': list(df[df.method == 'ModAddBig'].ns),
-        'Nat': list(df[df.method == 'ModAddNat'].ns),
-    })
+    preproc = pd.DataFrame(
+        {
+            "Bits": list(df[df.method == "ModAddBig"].bits),
+            "Big": list(df[df.method == "ModAddBig"].ns),
+            "Nat": list(df[df.method == "ModAddNat"].ns),
+        }
+    )
     ax = plt.subplot(2, 1, 1)
-    preproc.plot(x="Bits", y="Nat", ax=ax, legend=False)
-    plt.title('ModAdd Execution Time (Nat)')
+    preproc.plot(x="Bits", y="Nat", ax=ax, legend=False, xlabel="", ylabel="ns")
+    plt.title("ModAdd Execution Time (Nat)")
     ax = plt.subplot(2, 1, 2)
-    preproc.plot(x="Bits", y="Big", ax=ax, legend=False, color="r")
-    plt.title('ModAdd Execution Time (Big)')
+    preproc.plot(x="Bits", y="Big", ax=ax, legend=False, color="r", ylabel="ns")
+    plt.title("ModAdd Execution Time (Big)")
     plt.tight_layout()
-    plt.savefig('./.out/modadd.png')
+    plt.savefig("./.out/modadd.png")
     pass
 
+
 def expplot(df):
-    preproc = pd.DataFrame({
-        'Hamming Weight': list(df[df.method == 'ModExpBig'].bits),
-        'Big': list(df[df.method == 'ModExpBig'].ns),
-        'Nat': list(df[df.method == 'ModExpNat'].ns),
-    })
+    preproc = pd.DataFrame(
+        {
+            "Hamming Weight": list(df[df.method == "ModExpBig"].bits),
+            "Big": list(df[df.method == "ModExpBig"].ns),
+            "Nat": list(df[df.method == "ModExpNat"].ns),
+        }
+    )
     plt.clf()
     ax = plt.subplot(2, 1, 1)
-    preproc.plot(x="Hamming Weight", y="Nat", ax=ax, legend=False)
-    plt.title('Exp Execution Time (Nat)')
+    preproc.plot(
+        x="Hamming Weight", y="Nat", ax=ax, legend=False, xlabel="", ylabel="ns"
+    )
+    plt.title("Exp Execution Time (Nat)")
     ax = plt.subplot(2, 1, 2)
-    preproc.plot(x="Hamming Weight", y="Big", ax=ax, legend=False, color="r")
-    plt.title('Exp Execution Time (Big)')
-    plt.savefig('./.out/exp.png')
+    preproc.plot(
+        x="Hamming Weight", y="Big", ax=ax, legend=False, color="r", ylabel="ns"
+    )
+    plt.title("Exp Execution Time (Big)")
+    plt.savefig("./.out/exp.png")
 
 
 def main():
