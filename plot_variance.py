d58fd356e5aff8e7c64cdf7d3c6556265fff756e
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-26 21:08:27 +0200

    Update graph generation

--- plot_variance.py
+++ plot_variance.py
@@ -13,10 +13,12 @@ def modaddplot(df):
     )
     plt.clf()
     ax = plt.subplot(1, 1, 1)
-    preproc.plot(x="Bits", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns")
     preproc.plot(x="Bits", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="ns")
+    preproc.plot(x="Bits", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns")
     ax.figure.legend()
     plt.title("Execution time of Modular Addition")
+    fig = plt.gcf()
+    fig.set_size_inches(7.5, 4)
     plt.tight_layout()
     plt.savefig("./.out/modadd.png")
     pass
@@ -33,16 +35,18 @@ def expplot(df):
     plt.clf()
     ax = plt.subplot(1, 1, 1)
     preproc.plot(
-        x="Hamming Weight", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns",
+        x="Hamming Weight", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="ns",
         logy=False
     )
-    plt.title("Exp Execution Time (Nat)")
     preproc.plot(
-        x="Hamming Weight", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="ns",
+        x="Hamming Weight", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns",
         logy=False
     )
     ax.figure.legend()
     plt.title("Execution time of Exponentiation")
+    fig = plt.gcf()
+    fig.set_size_inches(7.5, 4)
+    plt.tight_layout()
     plt.savefig("./.out/exp.png")
 
 
