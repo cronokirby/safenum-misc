5b9b39d1609ee4125a63759954c0d20bba7da3c7
Lúcás Críostóir Meier <cronokirby@gmail.com>
2021-08-29 11:43:07 +0200

    Adjust plots to be more informative

--- plot_variance.py
+++ plot_variance.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 
 
 def modaddplot(df):
     preproc = pd.DataFrame(
         {
             "Bits": list(df[df.method == "ModAddBig"].bits),
-            "Go's big.Int": list(df[df.method == "ModAddBig"].ns),
-            "safenum": list(df[df.method == "ModAddNat"].ns),
+            "Go's big.Int": list(df[df.method == "ModAddBig"].ns / 1000),
+            "saferith": list(df[df.method == "ModAddNat"].ns / 1000),
         }
     )
     plt.clf()
     ax = plt.subplot(1, 1, 1)
-    preproc.plot(x="Bits", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="ns")
-    preproc.plot(x="Bits", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns")
-    ax.figure.legend()
-    plt.title("Execution time of Modular Addition")
+    preproc.plot(x="Bits", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="μs")
+    preproc.plot(x="Bits", y="saferith", ax=ax, legend=False, xlabel="", ylabel="μs")
+    plt.xlabel('Significant Bits')
+    plt.xticks(np.arange(0, 4 * 1024 + 1, 1024))
+    ax.figure.legend(bbox_to_anchor=(1.0, 1.06), loc="upper right")
+    plt.title("Execution time of Modular Addition with 2048 bit modulus", loc="left")
     fig = plt.gcf()
-    fig.set_size_inches(7.5, 4)
+    fig.set_size_inches(8.1, 4)
     plt.tight_layout()
-    plt.savefig("./.out/modadd.png")
+    plt.savefig("./.out/modadd.png", bbox_inches="tight")
     pass
 
 
@@ -28,26 +31,28 @@ def expplot(df):
     preproc = pd.DataFrame(
         {
             "Hamming Weight": list(df[df.method == "ModExpBig"].bits),
-            "Go's big.Int": list(df[df.method == "ModExpBig"].ns),
-            "safenum": list(df[df.method == "ModExpNat"].ns),
+            "Go's big.Int": list(df[df.method == "ModExpBig"].ns / 1000),
+            "saferith": list(df[df.method == "ModExpNat"].ns / 1000),
         }
     )
     plt.clf()
     ax = plt.subplot(1, 1, 1)
     preproc.plot(
-        x="Hamming Weight", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="ns",
+        x="Hamming Weight", y="Go's big.Int", ax=ax, legend=False, color="r", ylabel="μs",
         logy=False
     )
     preproc.plot(
-        x="Hamming Weight", y="safenum", ax=ax, legend=False, xlabel="", ylabel="ns",
+        x="Hamming Weight", y="saferith", ax=ax, legend=False, xlabel="", ylabel="μs",
         logy=False
     )
-    ax.figure.legend()
-    plt.title("Execution time of Exponentiation")
+    plt.xlabel('Number of 1 bits in exponent')
+    plt.xticks(np.arange(0, 64 + 1, 16))
+    ax.figure.legend(bbox_to_anchor=(1.0, 1.06), loc="upper right")
+    plt.title("Execution time of Exponentiation with 64 bit exponent, 2048 bit base", loc='left')
     fig = plt.gcf()
-    fig.set_size_inches(7.5, 4)
+    fig.set_size_inches(8.1, 4)
     plt.tight_layout()
-    plt.savefig("./.out/exp.png")
+    plt.savefig("./.out/exp.png", bbox_inches="tight")
 
 
 def main():
